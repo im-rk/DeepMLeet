f4e6bc88ed306543962154537fc1ec6cee28bc34
Ramkumar K R <123564651+im-rk@users.noreply.github.com>
2025-04-28 23:42:32 +0530

    Update matrix.jl

--- matrix.jl
+++ matrix.jl
@@ -12,14 +12,10 @@ function matrix_dot_vector(a::Array{<:Real,2}, b::Array{<:Real,1})
     return result
 end
 
-# ===== Example Input and Output =====
 a = [1 2 3;
      4 5 6]
 
 b = [7, 8, 9]
 
-# Call the function
 output = matrix_dot_vector(a, b)
-
-# Print the result
 println("The output is: ", output)
